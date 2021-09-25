59c864a1e29243da2a74d8b6d15daec43280718a
imadhyan <adhyanchawla500@gmail.com>
2021-09-25 22:43:00 +0530

    commit

--- theory.js
+++ theory.js
@@ -61,3 +61,88 @@ for(let i = 0; i < arr3.length; i++) {
 }
 
 console.log(me);
+
+
+//concatenation
+let str = "";
+let arr4 = ["red", "green", "white"];
+
+for(let i = 0; i < arr4.length; i++) {
+    str += arr4[i];
+}
+
+console.log(str);
+
+//sum of 2D arrays
+let arr5 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let sum = 0;
+
+for(let i = 0; i < arr5.length; i++) {
+    for(let j = 0; j < arr5[i].length; j++) {
+        sum += arr5[i][j];
+    }
+}
+console.log(sum);
+
+let k = 0;
+let arr6 = [];
+for(let i = 0; i < arr5.length; i++) {
+    for(let j = 0; j < arr5[0].length; j++) {
+        arr6.push(arr5[i][j]);
+    }
+}
+
+console.log(arr6);
+
+//print arr with every element inc by 2
+let arr7 = [1,2,3,4];
+
+// for(let i = 0; i < arr7.length; i++) {
+//     arr7[i] += 2;
+// } 
+
+console.log(arr7);
+
+//functions in JS
+function abc() {
+    console.log("hello world");
+}
+
+abc();
+
+//map function
+function print(d, i) {
+    console.log(d, i);
+}
+
+arr7.map(print);
+
+//addTwo
+function addTwo(d, i) {
+    return d + 2;
+}
+
+arr7 = arr7.map(addTwo);
+console.log(arr7);
+
+
+//given an array remove all the even items
+let arr8 = [1,2,3,4,5,6,7,8];
+let arr9 = [];
+
+// for(let i = 0; i < arr8.length; i++) {
+//     if(arr8[i] % 2 != 0) {
+//         arr9.push(arr8[i]);
+//     }
+// }
+
+// console.log(arr9);
+
+//filter method
+function removeEven(data, index) {
+    return data % 2 != 0;
+}
+
+arr9 = arr8.filter(removeEven);
+console.log(arr9);
+
